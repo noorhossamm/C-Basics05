ccd284bb13fc1e3612d1c59d1c2689dc18fd5513
noorhossamm <noormwafy78@gmail.com>
2026-07-29 06:37:23 +0300

    Question01

--- Assignment_03/Program.cs
+++ Assignment_03/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+
+        #region Question01
+        //static bool TryGetPrice(string title, out double price)
+        //{
+        //    if (title == "Clean Code")
+        //    {
+        //        price = 25.5;
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        price = 0;
+        //        return false;
+        //    }
+        //} 
+        #endregion
+
         static void Main(string[] args)
         {
-           
+            #region Question01
+            //double price;
+            //bool IsPriceFound = TryGetPrice("Clean Code", out price);
+            //Console.WriteLine($"Price :{price}"); 
+            #endregion
+
+
+
         }
     }
 }
